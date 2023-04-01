03e3a49fac8e902877abcf8b393de0b3f619f737
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-04-01 23:12:38 +0200

    Ingetrated database

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology
 
-# Create a database connection
-def create_connection(db_file):
+# Create a database
+def create_database(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -19,11 +19,50 @@ def create_connection(db_file):
             conn.close()
 
 if __name__ == '__main__':
-    create_connection(r"test.db")
+    create_database(r"test.db")
     
+# Create a database connection
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+# Create a table entry
+def create_user(conn, user):
+    sql = ("INSERT INTO users (username, hash) VALUES(?, ?)")
+    cur = conn.cursor()
+    cur.execute(sql, user)
+    conn.commit()
+    return cur.lastrowid
+
+def main():
+    database = (r"test.db")
+
+    # Create a database connection
+    conn = create_connection(database)
+    with conn:
+        # Create a new user
+        user = ('Test_user', 'Test_password')
+        create_user(conn, user)
+
+
+
 
 # Configure application
 app = Flask(__name__)
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# GET THIS SHIT WORKING
 
 @app.route("/", methods=["GET", "POST"])
 
@@ -38,3 +77,24 @@ def register():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
+    
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+
+
+
+
+
+
+
+main()
