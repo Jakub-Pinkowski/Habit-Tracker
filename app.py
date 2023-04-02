c499fb2624d4696c503f7e20ad32f61acf34346b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-04-02 14:16:37 +0200

    Register route is done

--- app.py
+++ app.py
@@ -6,55 +6,17 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology
 
-# Create a database
-def create_database(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print("e")
-    finally:
-        if conn:
-            conn.close()
 
-if __name__ == '__main__':
-    create_database(r"test.db")
-    
-# Create a database connection
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-# Create a table entry
-def create_user(conn, user):
-    sql = ("INSERT INTO users (username, hash) VALUES(?, ?)")
-    cur = conn.cursor()
-    cur.execute(sql, user)
-    conn.commit()
-    return cur.lastrowid
-
+# Main function
 def main():
-    database = (r"test.db")
-
-    # Create a database connection
-    conn = create_connection(database)
-    with conn:
-        # Create a new user
-        user = ('Test_user', 'Test_password')
-        create_user(conn, user)
-
+    
+    # To be done - it's just a PLACEHOLDER
+    print("Main funcion working now")
 
 
 
 # Configure application
 app = Flask(__name__)
-
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -62,26 +24,91 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-# GET THIS SHIT WORKING
+# Databeses functions
+
+database = (r"test.db")
+
+def create_connection(db_file):
+    """ Create a database connection to a SQLite database"""
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+def create_user(conn, user):
+    """ Create a new user into the users table"""
+    sql = ("INSERT INTO users (username, hash) VALUES(?, ?)")
+    cur = conn.cursor()
+    cur.execute(sql, user)
+    conn.commit()
+    return cur.lastrowid
+
+
+# Define routes
 
 @app.route("/", methods=["GET", "POST"])
-
 def index():
+    """ Homepage """
+
     return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """ Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+
+        # Create variables
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        hash_password = generate_password_hash(password)
+
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 400)
+        
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 400)
+        
+        # Ensure the confirmation matches the original password
+        elif password != confirmation:
+            return apology("passwords do not match", 400)
+        
+        # Check if username already exists
+        conn = create_connection(database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT username FROM users WHERE username = ?", (username,))
+            rows = cur.fetchall()
+            if rows:
+                return apology("username already exists", 400)
+
+        # Insert new user into database
+        conn = create_connection(database)
+        with conn:
+            user = (username, hash_password)
+            user_id = create_user(conn, user)
+
+        # Flash
+        flash("Registered!")
+
+        # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
     
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """ Log user in"""
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         return redirect("/")
 
--- helpers.py
+++ helpers.py
@@ -14,3 +14,13 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """ Decorate routes to require login. """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
