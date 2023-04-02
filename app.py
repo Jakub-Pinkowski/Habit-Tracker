a0624e4f65b2ef32ae7dfdb4403a9d41c624e193
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-04-02 15:06:36 +0200

    Log in and Log out implemented

--- app.py
+++ app.py
@@ -108,14 +108,53 @@ def register():
 def login():
     """ Log user in"""
 
+    # Forget any user_id
+    session.clear()
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        return redirect("/")
 
+        # Create variables
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+            
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+        
+        # Query database for username
+        conn = create_connection(database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+            rows = cur.fetchall()
+            if len(rows) != 1 or not check_password_hash(rows[0][2], password):
+                return apology("invalid username and/or password", 400)
+            
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+        print(rows[0][0])
+
+        # Redirect user to home page
+        return redirect("/") 
+    
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    """ Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
 
